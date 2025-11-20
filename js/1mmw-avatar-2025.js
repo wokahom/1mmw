4b6b284b167f3601ab2e69bad2528b8a51213cc9
Zerubbabel CNJ <wokahom@gmail.com>
2025-11-20 13:24:02 +0100

    battery low

--- js/1mmw-avatar-2025.js
+++ js/1mmw-avatar-2025.js
@@ -5,7 +5,7 @@ const downloadBtn = document.getElementById("downloadBtn");
 
 // Template
 const template = new Image();
-template.src = "assets/images/template_transparent.png";
+template.src = "assets/images/template_transparent_1.png";
 
 // Circle coordinates
 const circleX = 400;
